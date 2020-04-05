6cabd366582afb05269cabf3a966181fa6e5f5dc
Aryaman-7 <62686179+Aryaman-7@users.noreply.github.com>
2020-04-05 11:56:38 +0530

    Add files via upload

--- Covexa V1.2.py
+++ Covexa V1.2.py
@@ -31,43 +31,43 @@ if 'hello' in words:
     engine.runAndWait() 
 
 if "sanitizer" in words :
-    print ( "You should use an alcoholic hand sanitizer to kill germs")
+
     engine.say("You should use an alcoholic hand sanitizer to kill germs")
     engine.runAndWait()
 
 if "what" and "corona virus" in words :
-    print("It is a new virus that has a mortality rate of 1.38%")
+
     engine.say("It is a new virus that has a mortality rate of 1.38%")
     engine.runAndWait()
    
 if "what" and "symptoms" and "corona virus" in words :
-    print ("The symptoms of COVID-19 include dry cough, fever, fatigue,Severe Acute Respiratory Sindrome ,etc")
+
     engine.say("The symptoms of Covid nineteen include dry cough, fever, fatigue, Severe Acute Respiratory Sindrome, etc")
     engine.runAndWait()
        
        
 if "cure" and "corona virus" in words :
-    print ("A cure to Corona Virus is not found yet")
+
     engine.say ("A cure to Corona Virus is not found yet")
     engine.runAndWait()
        
 if "prevent" and "corona virus" in words :
-    print ("You should always wash your hands with soap and water, you should not restrain from touching things and quarantine yourself if you are ill and if you are in a Corona Virus Affrected area")
+
     engine.say("You should always wash your hands with soap and water, you should not restrain from touching things and quarantine yourself if you are ill and if you are in a Corona Virus Affrected area")
     engine.runAndWait()
        
 if "cases"   and "worldwide"  in words  :
-    print ("There are 803547 cases of corona virus worldwide as of 7:20 IST, 31st March,2020")
+
     engine.say ("There are 803547 cases of corona virus worldwide as of 7:20 IST, 31st March,2020")
     engine.runAndWait()
    
 if "cases" and "india" in words :
-    print ("There are 1117 cases of Corona Virus in India as of 7:34 IST, 31st March,2020")
+
     engine.say("There are 1117 cases of Corona Virus in India as of 7:34 IST, 31st March,2020")
     engine.runAndWait()
        
 if "bye" in words :
-    print("Bye, hope you are well buddy!")
+
     engine.say("Bye, hope you are well buddy!")
     engine.runAndWait()
     engine.stop()
